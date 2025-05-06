991b3a0c77d4398d8b986ca560a865596f5fe4ce
Paulina <paulii_93@live.se>
2025-05-06 23:42:26 +0200

    Add files via upload

---
+++ PC-fil.js
@@ -0,0 +1,72 @@
+[
+    {
+        "_id": "6793b7fb56a7fe50723f5b6f",
+        "brand": "Levono",
+        "model": "Legion Tower 7i Gen 8",
+        "price": "13k",
+        "processor": "Intel Core i9-14900KF",
+        "graphicscard": "Nvidia GeForce RTX 4070 Ti Super"
+    },
+    {
+        "_id": "6793b99b56a7fe50723f5b70",
+        "brand": "Acer",
+        "model": "Aspire TC-1780-UA92",
+        "price": "6,5k",
+        "processor": "Intel Core i5 processor ",
+        "graphicscard": "Intel UHD Graphics 730"
+    },
+    {
+        "_id": "6793ba5656a7fe50723f5b71",
+        "brand": "MSI",
+        "model": "Codex R2",
+        "price": "9,8K",
+        "processor": "Intel Core i5-14400F",
+        "graphicscard": "Nvidia GeForce RTX 4060"
+    },
+    {
+        "_id": "6793bad556a7fe50723f5b72",
+        "brand": "Corsair",
+        "model": "Vengeance i7500",
+        "price": "19K",
+        "processor": "Intel Core i9-14900K",
+        "graphicscard": "Nvidia GeForce RTX 4090 "
+    },
+    {
+        "_id": "6793bbe256a7fe50723f5b73",
+        "brand": "iBuyPower",
+        "model": "iBuyPower Y60",
+        "price": "14k",
+        "processor": "AMD Ryzen 7 7700X",
+        "graphicscard": "GeForce RTX 4060 Ti Verto"
+    },
+    {
+        "_id": "6793bcaa56a7fe50723f5b74",
+        "brand": "Maingear",
+        "model": "MG-1 Silver",
+        "price": "12k",
+        "processor": "Intel Core i5-13400F",
+        "graphicscard": "PNY GeForce RTX 4060"
+    },
+    {
+        "_id": "6793bd1556a7fe50723f5b75",
+        "brand": "HP ",
+        "model": "Omen 45L",
+        "price": "35k",
+        "processor": "Intel® Core™ i7 - 14700K",
+        "graphicscard": "NVIDIA® GeForce® RTX™ 4080 SUPER"
+    },
+    {
+        "_id": "6793bdfd56a7fe50723f5b76",
+        "brand": "White Shark",
+        "model": "Massacre",
+        "price": "19k",
+        "processor": "AMD Ryzen 5 9600X",
+        "graphicscard": "Gigabyte GeForce RTX 4070 Super Eagle"
+    },
+    {
+        "_id": "681023a07b434eb36ec11496",
+        "brand": "Lexus",
+        "model": "Qwuie",
+        "__v": 0
+    }
+]
